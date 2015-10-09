b42c01ba8aa3e999f525b307af31c2a2fee8133a
Friedrich Lindenberg <friedrich@pudo.org>
2015-10-09 09:35:34 +0200

    play with threaded loading

--- loom/db/writer.py
+++ loom/db/writer.py
@@ -3,6 +3,9 @@ import unicodecsv
 import tempfile
 from time import time
 
+from threading import Thread
+from Queue import Queue
+
 log = logging.getLogger(__name__)
 
 
@@ -13,31 +16,52 @@ class Writer(object):
     def __init__(self, manager, engine):
         self.manager = manager
         self.engine = engine
-        self.temp = tempfile.NamedTemporaryFile(suffix='.csv')
         self.fields = [unicode(c.name) for c in self.manager.columns]
         self.fields = [f for f in self.fields if f not in ['id', 'timestamp']]
-        self.writer = unicodecsv.DictWriter(self.temp, fieldnames=self.fields)
         self.rows = 0
+        self.create_file()
 
-    def flush(self):
-        self.temp.flush()
-        self.temp.seek(0)
+        self.queue = Queue()
+        self.thread = Thread(target=self.loader_thread)
+        self.thread.daemon = True
+        self.thread.start()
+
+    def create_file(self):
+        self.temp = tempfile.NamedTemporaryFile(suffix='.csv')
+        self.writer = unicodecsv.DictWriter(self.temp, fieldnames=self.fields)
+
+    def loader_thread(self):
+        while True:
+            temp = self.queue.get()
+            self.bulk_load(temp)
+            self.queue.task_done()
+
+    def bulk_load(self, temp):
+        temp.flush()
+        temp.seek(0)
         begin = time()
         raw_conn = self.engine.raw_connection()
-        log.info("Bulk loading %s rows into %r", self.rows, self.manager.name)
+        log.info("Bulk loading into %r (ca. %s)", self.manager.name, self.rows)
         cur = raw_conn.cursor()
         q = """
             COPY %s (%s) FROM STDIN
                 WITH CSV HEADER DELIMITER ',' QUOTE '\"'
                 ENCODING 'utf-8'
         """ % (self.manager.name, ', '.join(self.fields))
-        cur.copy_expert(q, self.temp)
+        cur.copy_expert(q, temp)
         raw_conn.commit()
         cur.close()
         duration = (time() - begin)
         log.info("COPY done after %.2fs", duration)
-        self.temp.close()
+        temp.close()
+
+    def flush(self):
+        self.queue.put(self.temp)
+        self.queue.join()
 
     def write(self, record):
         self.writer.writerow(record)
         self.rows += 1
+        if self.rows > 0 and self.rows % 500000 == 0:
+            self.queue.put(self.temp)
+            self.create_file()
