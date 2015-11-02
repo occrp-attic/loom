a0898037f466022f344e35bb9113fcec9ab81df2
Friedrich Lindenberg <friedrich@pudo.org>
2015-11-02 10:16:19 +0100

    Add collection

--- loom/db/model.py
+++ loom/db/model.py
@@ -92,3 +92,20 @@ class Source(Base):
 
     def __repr__(self):
         return '<Source(%s)>' % self.slug
+
+
+class Collection(Base):
+    """ A collection of entities. """
+    __tablename__ = 'collection'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(Unicode())
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title
+        }
+
+    def __repr__(self):
+        return '<Collection(%s)>' % self.title
